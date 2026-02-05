f13c85ffcef54ac98382cd6e428d3e525d185990
Indro <indronath.basu@gmail.com>
2026-02-05 13:37:28 +0530

    Resolved merge conflict from revert

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,19 +2,17 @@ import Homepage from "./components/Homepage";
 import { Route, Routes } from "react-router-dom";
 import AdminDashboard from "./components/admin";
 import Login from "./components/login";
-import Homepage from "./components/Homepage";
 import AdminLogin from "./components/adminlogin";
 
 const App = () => {
-return (
-    <Routes>
+    return (
+        <Routes>
             <Route path="/" element={<Form />} />
-            <Route path="/" element={<Homepage />} />
             <Route path="/form" element={<Form />} />
             <Route path="/login" element={<Login />} />
             <Route path="/admin" element={<AdminDashboard />} />
             <Route path="/login" element={<AdminLogin />} />
-    </Routes>
-);
+        </Routes>
+    );
 };
 export default App;
