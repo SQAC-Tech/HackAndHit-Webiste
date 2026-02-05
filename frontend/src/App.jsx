3d739123b5ee403414b65d40c8b642c1d8318688
SQAC (Software Quality Assurance Community) <sqac.club@gmail.com>
2026-02-05 13:17:37 +0530

    Add AdminLogin route to the application

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,17 @@
-import React from "react";
-import { Route, Routes } from "react-router";
-import Form from "./components/form";
 import Homepage from "./components/Homepage";
 import AdminDashboard from "./components/admin";
 import Login from "./components/login";
+import AdminLogin from "./components/adminlogin";
 
 const App = () => {
-    return (
-        <Routes>
+return (
+    <Routes>
+            <Route path="/" element={<Form />} />
             <Route path="/" element={<Homepage />} />
             <Route path="/form" element={<Form />} />
             <Route path="/login" element={<Login />} />
             <Route path="/admin" element={<AdminDashboard />} />
-        </Routes>
-    );
+            <Route path="/login" element={<AdminLogin />} />
+    </Routes>
+);
 };
-
-export default App;
