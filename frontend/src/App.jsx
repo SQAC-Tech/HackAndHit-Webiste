f554c47f57fc280d3e88eb4e8dc9555aa96a24ab
SQAC (Software Quality Assurance Community) <sqac.club@gmail.com>
2026-02-05 13:02:01 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,16 +4,14 @@ import Form from "./components/form";
 import Homepage from "./components/Homepage";
 import AdminDashboard from "./components/admin";
 import Login from "./components/login";
-import AdminLogin from "./components/adminlogin";
 
 const App = () => {
     return (
         <Routes>
-            <Route path="/" element={<Form />} />
+            <Route path="/" element={<Homepage />} />
             <Route path="/form" element={<Form />} />
             <Route path="/login" element={<Login />} />
             <Route path="/admin" element={<AdminDashboard />} />
-            <Route path="/login" element={<AdminLogin />} />
         </Routes>
     );
 };
