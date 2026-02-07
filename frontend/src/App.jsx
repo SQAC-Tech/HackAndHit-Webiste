06e0acf29b0ea5a293a3fdd811e9437923d99be6
Amisha <pampexam@gmail.com>
2026-02-07 22:20:09 +0530

    Added FAQ page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import Form from "./components/form";
 import AdminLogin from "./components/adminlogin";
 import MainSite from "./components/MainSite";
 import Footer from "./components/Footer";
-
+import FAQ from "./components/FAQ";
 function App() {
   return (
     <>
@@ -16,6 +16,7 @@ function App() {
         <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/adminlogin" element={<AdminLogin />} />
         <Route path="/mainsite" element={<MainSite />} />
+        <Route path="/faq" element={<FAQ />} />
       </Routes>
 
       {/* Footer will appear on all pages */}
