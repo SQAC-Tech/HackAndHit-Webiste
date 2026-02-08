0b4cca2489793edbef2faea984e400abc99b9eab
MJ-1112 <m22327803@gmail.com>
2026-02-08 12:15:56 +0530

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,8 +11,8 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Form />} />
-        <Route path="/home" element={<MainSite />} />
+        <Route path="/" element={<MainSite />} />
+        <Route path="/form" element={<Form />} />
         <Route path="/login" element={<Login />} />
         <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/adminlogin" element={<AdminLogin />} />
