83b9d7a4c651e1e244f8a6b09aa636c00b5107ea
Mahik <m22327803@gmail.com>
2026-02-12 08:59:02 +0530

    Update App.jsx
    closed the team login and ppt submission form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,8 +6,8 @@ import AdminLogin from "./components/adminlogin";
 import MainSite from "./components/MainSite";
 import Footer from "./components/Footer";
 import FAQ from "./components/FAQ";
-import TeamLogin from "./components/teamlogin";
-import PPTSubmit from "./components/pptsubmit";
+{/*import TeamLogin from "./components/teamlogin";
+import PPTSubmit from "./components/pptsubmit";*/}
 import AdminMailer from "./components/massmailer";
 
 function App() {
@@ -16,14 +16,14 @@ function App() {
       <Routes>
         <Route path="/" element={<MainSite />} />
         {/* <Route path="/form" element={<Form />} /> */}
-        <Route path="/submitppt" element={<TeamLogin />} />
+        {/*<Route path="/submitppt" element={<TeamLogin />} />*/}
         <Route path="/login" element={<Login />} />
         <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/adminlogin" element={<AdminLogin />} />
         <Route path="/mainsite" element={<MainSite />} />
         <Route path="/faq" element={<FAQ />} />
-        <Route path="/teamlogin" element={<TeamLogin />} />
-        <Route path="/ppt-submit" element={<PPTSubmit/>} />
+       {/* <Route path="/teamlogin" element={<TeamLogin />} />*/}
+        {/*<Route path="/ppt-submit" element={<PPTSubmit/>} />*/}
         <Route path="/mailler" element={<AdminMailer />} />
       </Routes>
     </>
